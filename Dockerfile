3b47849b358aa1bbf5b43dbbbc9ce54c3b5ec48d
Kai Wohlfahrt <kai@charmtx.com>
2023-11-22 23:24:00 +0100

    Credential output
    The authentication server has two endpoints. This implements (1):
    1. A proxy for `.well-known/openid-configuration` of the upstream
       authentication server.
    
    Next will be (2):
    2. A token endpoint. This simply echoes back the password header as the
       token. It doesn't look like the docker client supports sending the
       password as an OAuth token, so we need this wrapper.

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ from python:3
 WORKDIR /app
 COPY ./server.py ./server.py
 
+STOPSIGNAL SIGTERM
 ENTRYPOINT ["python3", "./server.py"]
--- server.py
+++ server.py
@@ -1,21 +1,48 @@
 #!/usr/bin/env python3
 
+import json
+import signal
+import ssl
 import sys
+from threading import Thread
 from argparse import ArgumentParser
+from functools import cached_property
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import urlparse, urlunparse
+from urllib.request import urlopen
+
+
+ssl_ctx = ssl.create_default_context()
+
+# FIXME: This is for testing, remove
+ssl_ctx.check_hostname = False
+ssl_ctx.verify_mode = ssl.CERT_NONE
 
 
 class Server(HTTPServer):
-    def __init__(self, upstream: str, *args, **kwargs):
-        self.upstream = upstream
+    def __init__(self, realm: str, *args, **kwargs):
+        self.realm = urlparse(realm)
         super().__init__(*args, **kwargs)
 
 
 class Handler(BaseHTTPRequestHandler):
+    def do_HEAD(self):
+        self.log_message("HEAD %s %s", self.path, self.headers)
+
+    def do_POST(self):
+        self.log_message("POST %s %s", self.path, self.headers)
+
     def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write("Hello, world!".encode())
+        path = urlparse(self.path)
+        if path.path == "/auth/.well-known/openid-configuration":
+            self.send_response(303)
+            redirect = self.server.realm._replace(
+                path="/".join([self.server.realm.path, ".well-known", "openid-configuration"])
+            )
+            self.send_header("Location", urlunparse(redirect))
+            self.end_headers()
+        else:
+            self.log_message("GET %s %s", self.path, self.headers)
 
 
 def main(argv=None):
@@ -23,11 +50,15 @@ def main(argv=None):
         argv = sys.argv[1:]
 
     parser = ArgumentParser()
-    parser.add_argument("upstream", type=str)
+    parser.add_argument("realm", type=str)
     args = parser.parse_args(argv)
 
-    httpd = Server(args.upstream, ('', 80), Handler)
-    httpd.serve_forever()
+    httpd = Server(args.realm, ('', 80), Handler)
+    signal.signal(signal.SIGTERM, lambda *args: httpd.shutdown())
+
+    t = Thread(target=httpd.serve_forever)
+    t.start()
+    t.join()
 
 if __name__ == "__main__":
     main()
